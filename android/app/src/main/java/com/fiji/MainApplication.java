31bd2c6fd71a2a7431e7c2d4d7a5e0bad108e4b8
Srikanth Talapadi <sri@zoolore.org>
2018-01-12 10:08:15 +0530

    Added image as base 64 encoded text in pouchdb

--- android/app/src/main/java/com/fiji/MainApplication.java
+++ android/app/src/main/java/com/fiji/MainApplication.java
@@ -3,6 +3,7 @@ package com.fiji;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
            new VectorIconsPackage(),
 
             new RNSoundPackage(),
