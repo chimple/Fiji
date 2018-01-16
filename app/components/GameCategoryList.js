942f1bf98d42efe390691ea459cbf071d3c9b9b5
aayush-learning <mirus.aayush@gmail.com>
2018-01-16 17:32:00 +0530

    Edits to GameCategoryList.
    Added two buttons on either side of the list.

--- app/components/GameCategoryList.js
+++ app/components/GameCategoryList.js
@@ -5,6 +5,20 @@ import PropTypes from 'prop-types'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export default class GameCategoryList extends PureComponent {
+
+ /* _scrollToForwardIndex = () => (
+
+  )
+
+  _scrollToBackwardIndex = () => (
+
+  )*/
+
+  myscrollToIndex = () => {
+    this.setState({index: ++this.state.index});
+    this.flatListRef.scrollToIndex({animated: true,index: this.state.index});
+  };
+
   _keyExtractor = (item, index) => item._id
 
   _renderItem = ({item}) => (
@@ -17,23 +31,24 @@ export default class GameCategoryList extends PureComponent {
       </View>
       <View style={styles.CategoryGamesViewStyle}>
           <View style={styles.IconViewStyle}> 
-            <Icon name="keyboard-arrow-left" color="white" size={35} style={styles.IconStyle} />
+            <Icon name="keyboard-arrow-left" color="white" size={35} style={styles.IconStyle} onPress={this._scrollToForwardIndex} />
           </View>
-          <View style={{flex:10, }}>             
+          <View style={{flex:12, }}>               
           <FlatList
           showsHorizontalScrollIndicator={false}
+          ref={(ref) => { this.flatListRef = ref; }}
           horizontal={true}
           data={item.games}
           keyExtractor={this._keyExtractor}
-          renderItem={({item}) => (
-            <TouchableOpacity style={styles.EachGameButtonStyle}>
-              <View style={styles.EachGameViewStyle}/>
-            </TouchableOpacity>
+          renderItem={({item, index}) => (
+            <View style={{   paddingHorizontal:'0.5%' }}>
+              <TouchableOpacity style={styles.EachGameViewStyle}/>
+            </View>
           )}
           />
           </View>
           <View style={styles.IconViewStyle}>
-            <Icon name="keyboard-arrow-right" color="white" size={35} style={styles.IconStyle} />
+            <Icon name="keyboard-arrow-right" color="white" size={35} style={styles.IconStyle} onPress={this._scrollToBackwardIndex} />
           </View>
       </View>
     </View>
@@ -88,8 +103,9 @@ const styles = StyleSheet.create({
   CategoryGamesViewStyle:{
     flex:3.5 ,
     //padding:'4%',
-    paddingTop:'4%',
-    paddingBottom:'4%',
+    paddingTop:'7%',
+    paddingBottom:'7%',
+    //paddingBottom:'4%',
     backgroundColor:'#eaefef',
     flexDirection:'row',
     //justifyContent:'center', 
@@ -97,33 +113,40 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius:20, 
     borderBottomRightRadius:20
   },
-  EachGameButtonStyle:{ 
-    justifyContent:'center',
+  /*EachGameButtonStyle:{ 
+    //justifyContent:'center',
     //alignItems:'center',
     //alignContent:"space-around",
     //marginHorizontal:'2%,
-    //backgroundColor:'red',
+    backgroundColor:'red',
     //borderWidth:4,
-    borderRadius:40,
-   },
+    //borderRadius:50,
+    paddingRight:'2%',
+    paddingLeft:'2%'
+   },*/
   EachGameViewStyle:{
-    borderColor:'black',  
-    borderWidth:2,
+    //borderColor:'black',  
+    //borderWidth:1,
+    //justifyContent:'center',
     borderRadius:40, 
-    height:70, 
-    width:70,
+    height:85, 
+    width:85,
     backgroundColor:'black',
+    borderWidth:2,
   },
-  TextStyle:{
+  /*TextStyle:{
     fontSize:20,
     fontWeight:'bold',
     color:'black',
-  },
+  },*/
   IconViewStyle:{
     flex:1,
     justifyContent:"center",
-    alignItems:"center",
-    paddingRight:'3%'
+    //alignItems:"center",
+    paddingRight:'3%',
+    //backgroundColor:'black',
+    marginBottom:'5%',
+    marginTop:'5%'
   },
   IconStyle:{
     color:'#19a4f2'
@@ -135,4 +158,11 @@ const styles = StyleSheet.create({
 <TouchableOpacity style={styles.EachGameButtonStyle}>
             <Text style={styles.TextStyle}>{item.name}</Text>
           </TouchableOpacity>
+*/
+
+/*
+<TouchableOpacity style={styles.EachGameButtonStyle}>
+  <View style={styles.EachGameViewStyle}/>
+</TouchableOpacity>
+
 */
