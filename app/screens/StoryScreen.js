9f514cb8ef70a6546d62c5e922079c7dd3dd3057
Manukumar1995 <35102864+Manukumar1995@users.noreply.github.com>
2018-01-16 12:46:01 +0530

    FIJI-37 story screen updated

--- app/screens/StoryScreen.js
+++ app/screens/StoryScreen.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, Image, ImageBackground, ScrollView } from 'react-native'
+import { View, Text, ActivityIndicator, Image, ImageBackground, ScrollView, TouchableOpacity, FlatList } from 'react-native'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-
+import { Icon } from 'react-native-elements'
 import { fetchStory } from '../redux/story'
 import SvgUri from 'react-native-svg-uri'
 
+
 class StoryScreen extends Component {
-  componentDidMount() {
+ 
+ componentDidMount() {
     this.props.dispatch(fetchStory(this.props.navigation.state.params.title))
   }
+   _storyview = () => {
+     const stories=[];
+     const temp=null;
+       for(let i=0; i<6; i++)
+       {
+      if (this.props.story.characters['Alice'] === this.props.story.pages[0].dialog[i].speaker){ 
+        console.log("this is alice");
+        stories.push(
+      <View style={styles.storyContainer}>
+        <Image style={styles.characterImageStyle}
+          source={{
+            uri:
+              'data:image/png;base64,' + this.props.story.pages[0].dialog[i].image,
+          }} />
+        <View style={styles.textContainer}>
+          <Text style={styles.dialogStyle}>
+            {this.props.story.pages[0].dialog[i].text}
+          </Text>
+        </View>
+      </View>
+      );
+    }
+      else {
+        console.log("this is white ra vmnkjafj");
+        stories.push(
+      <View style={styles.storyContainer2}>
+        <View style={styles.textContainer}>
+          <Text style={styles.dialogStyle}>
+            {this.props.story.pages[0].dialog[i].text}
+          </Text>
+        </View>
+        <Image style={styles.characterImageStyle}
+          source={{
+            uri:
+              'data:image/png;base64,' + this.props.story.pages[0].dialog[i].image,
+          }} />
+      </View>
+    );
+  }
+    }
+    return stories;
+    console.log("comingggggggggggggg");
+    console.log(stories);
+    // return storys;
+  }
+    
 
-  render() {
-    console.log("coming")
-    console.log(this.props.story)
+render() {
+
     return (
       this.props.isFetching
         ?
@@ -38,35 +85,18 @@ class StoryScreen extends Component {
               <ImageBackground style={styles.backgroundImageStyle}
                 source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaBVHZKXKvVZepsZRShbLhp-QwShGWlRn6OkMCddQBBsD73ujE' }}>
                 {/* {this.props.story.pages[0].bg} */}
-                <ScrollView>
-                  <View style={styles.storyContainer}>
-                    <Image style={styles.characterImageStyle}
-                      source={{
-                        uri:
-                          'data:image/png;base64,' + this.props.story.pages[0].dialog[0].image,
-                      }} />
-                    <View style={styles.textContainer}>
-                      <Text style={styles.dialogStyle}>
-                        {this.props.story.pages[0].dialog[0].text}
-                      </Text>
-                    </View>
-                  </View>
-
-                  <View style={styles.storyContainer2}>
-                    <View style={styles.textContainer}>
-                      <Text style={styles.dialogStyle}>
-                        {this.props.story.pages[0].dialog[1].text}
-                      </Text>
-                    </View>
-                    <Image style={styles.characterImageStyle}
-                      source={{
-                        uri:
-                          'data:image/png;base64,' + this.props.story.pages[0].dialog[1].image,
-                      }} />
-                  </View>
-                </ScrollView>
+              <ScrollView>
+                <View>
+              {this._storyview()}
+              </View>
+            </ScrollView>
               </ImageBackground>
             </View>
+            <TouchableOpacity>
+              <View style={styles.nextButtonStyle}>
+                <Icon name='keyboard-arrow-down' color='black' />
+              </View>
+            </TouchableOpacity>
           </View>
           :
           <View>
@@ -77,6 +107,7 @@ class StoryScreen extends Component {
 }
 
 StoryScreen.propTypes = {
+  story: PropTypes.object,
   navigation: PropTypes.shape({
     state: PropTypes.shape({
       params: PropTypes.shape({
@@ -109,6 +140,7 @@ const styles = {
     flexDirection: 'row',
     alignItems: 'flex-end',
     margin: 5,
+    flexWrap: 'wrap'
 
   },
   textContainer: {
@@ -144,9 +176,57 @@ const styles = {
     alignSelf: 'flex-end',
 
   },
+  nextButtonStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#9999ff',
+    height: 30,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.9,
+    position: 'relative',
+    opacity: 1
+  }
 }
 
 export default connect(state => ({
   story: state.story.story,
   isFetching: state.story.isFetching,
 }))(StoryScreen)
+
+
+
+
+//_renderDialogLeft() {
+  //   if (this.props.story.characters['Alice'] === this.props.story.pages[0].dialog[0].speaker) {
+  //     <View style={styles.storyContainer}>
+  //       <Image style={styles.characterImageStyle}
+  //         source={{
+  //           uri:
+  //             'data:image/png;base64,' + this.props.story.pages[0].dialog[0].image,
+  //         }} />
+  //       <View style={styles.textContainer}>
+  //         <Text style={styles.dialogStyle}>
+  //           {this.props.story.pages[0].dialog[0].text}
+  //         </Text>
+  //       </View>
+  //     </View>
+  //   }
+  // }
+  // _renderDialogRight() {
+  //   return (
+  //     <View style={styles.storyContainer2}>
+  //       <View style={styles.textContainer}>
+  //         <Text style={styles.dialogStyle}>
+  //           {this.props.story.pages[0].dialog[1].text}
+  //         </Text>
+  //       </View>
+  //       <Image style={styles.characterImageStyle}
+  //         source={{
+  //           uri:
+  //             'data:image/png;base64,' + this.props.story.pages[0].dialog[1].image,
+  //         }} />
+  //     </View>
+  //   )
+  // }
