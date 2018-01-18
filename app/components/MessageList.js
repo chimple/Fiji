9b25d50ac581d76a33229e50cd02dea7446a0ada
kiran kumar bv <30715570+KiranChimple@users.noreply.github.com>
2018-01-18 18:36:44 +0530

    chat updated solving issues
    clear text and blank message

--- app/components/MessageList.js
+++ app/components/MessageList.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 import ChatView from './ChatView'
 import { addMessage,sendMessage } from '../redux/chat'
 import users from '../redux/users';
+import {Icon} from 'react-native-elements'
 
 
 
@@ -30,6 +31,13 @@ export default class MessageList extends PureComponent {
     </View>
    
   )
+  clearText =()=>{
+    if(this.state.message !== '' ){
+    this._textInput.clear();
+    this.props.onPress(this.state.message);
+    this.setState({ message: '' });
+    }
+  }
 
   render() {
     console.log("sendMessage");
@@ -51,20 +59,20 @@ export default class MessageList extends PureComponent {
       />
        <View style={styles.input}>
       <TextInput
-
-              placeholder="please type here"
-              style={{ flex: 1 }}
-              value={this.state.message}
-              clearButtonMode = 'while-editing'
-              blurOnSubmit={true}
-              clearTextOnFocus={true}
-              onEndEditing={(message) => this.clear({message})}
-              onSubmitEditing={(message) => this.clear({message})}
-              onChangeText={(message) => this.setState({ message })}
-          />
-          <TouchableOpacity onPress={() => this.props.onPress(this.state.message)}>
-         <Image source={{uri:'https://d30y9cdsu7xlg0.cloudfront.net/png/1054386-200.png'}} 
-          onPress={() => this.props.onPress(this.state.message)} style={{height:40,width:40 }}/>
+                ref={component => this._textInput = component}
+                placeholder="please type here"
+                style={{ flex: 1 }}
+                value={this.state.message}
+                clearButtonMode='while-editing'
+                blurOnSubmit={true}
+                clearTextOnFocus={true}
+                onSubmitEditing={this.clearText}
+                onChangeText={(message) => this.setState({ message })}
+                          />
+        
+          <TouchableOpacity onPress={this.clearText}>
+         <Icon name="send"
+         size={40} color="#900"/>
           </TouchableOpacity>
 </View>
 </View>
@@ -78,21 +86,19 @@ export default class MessageList extends PureComponent {
         <View >
         <View style={{backgroundColor:"#ffffff",height:"100%",justifyContent:"flex-end"}}>
       <View style={styles.input}>
-      <TextInput
-
-              placeholder="please type here"
-              style={{ flex: 1 }}
-              clearButtonMode = 'while-editing'
-              blurOnSubmit={true}
-              value={this.state.message}
-              ref={input => { this.textInput = input }}
-              // onSubmitEditing={() => sendMessage.bind(this.props.friend,this.state.message)}
-              onChangeText={(message) => this.setState({ message })}
-/>
-<TouchableOpacity onPress={() => this.props.onPress(this.state.message)}>
-         <Image source={{uri:'https://d30y9cdsu7xlg0.cloudfront.net/png/1054386-200.png'}} 
-          onPress={() => this.props.onPress(this.state.message)} style={{height:40,width:40 }}/>
-          </TouchableOpacity>
+      <TextInput  
+                ref={component => this._textInput = component}
+                placeholder="please type here"
+                style={{ flex: 1 }}
+                value={this.state.message}
+                clearButtonMode='while-editing'
+                blurOnSubmit={true}
+                clearTextOnFocus={true}
+                onSubmitEditing={this.clearText}
+                onChangeText={(message) => this.setState({ message })}
+                          />
+        <Icon name="send" size={40} color="#900" />       
+        
 </View>
 </View>
 </View>
@@ -116,12 +122,12 @@ input: {
   // alignItems: 'flex-end',
   justifyContent:"flex-end",
   alignItems:"flex-end",
-  padding: 10,
+  // padding: 10,
   // height: 60,
-  width: "90%",
-  backgroundColor: '#fffa12',
-  margin: 10,
-  borderRadius: 30,
+  width: "100%",
+  backgroundColor: '#e3d0ca',
+  // margin: 10,
+  // borderRadius: 30,
   shadowColor: '#3d3d3d',
   shadowRadius: 2,
   shadowOpacity: 0.5,
