4784467ef4b60c39e80994eb85769da9823eff5f
shantkumar <shant@sutara.org>
2018-01-24 14:04:54 +0530

    created ConnectDots

--- app/screens/ConnectDotsScreen.js
+++ app/screens/ConnectDotsScreen.js
@@ -18,9 +18,15 @@ export default class ConnectDots extends Component {
       console.log(this.state.count)
      
       const items = [
-       
+        { name: '1', code: '#1abc9c' }, { name: '2', code: '#2ecc71' },
+        { name: '9', code: '#3498db' }, { name: '', code: '#9b59b6' },
+        { name: '3', code: '#34495e' }, { name: '8', code: '#16a085' },
+        { name: '', code: '#27ae60' }, { name: '4', code: '#2980b9' },
+        { name: '7', code: '#8e44ad' }, { name: '', code: '#2c3e50' },
+       { name: '5', code: '#f1c40f' }, { name: '6', code: '#e67e22' },
         
       ];
+  
   
       return (
         <GridView
