a0fd45494db827b1052131cb2a2710c70c6872b1
sutara-aayush <35102016+sutara-aayush@users.noreply.github.com>
2018-01-24 09:02:37 +0530

    Error and duplicate settings removed.
    I was having error while trying to run the code on my system so i just edited those errors and corrected it .

--- app/db.js
+++ app/db.js
@@ -27,8 +27,8 @@ import PouchDB from 'pouchdb-react-native';
 // })
 
  //localhost:5984 to run in local server
-//const remoteURL = 'http://localhost:5984/'
-const remoteURL = 'http://192.168.0.200:5984/'
+const remoteURL = 'http://localhost:5984/'
+//const remoteURL = 'http://192.168.0.200:5984/'
 
 const usersDB = new PouchDB('users')
 const remoteUsersDB = new PouchDB(remoteURL + 'users')
