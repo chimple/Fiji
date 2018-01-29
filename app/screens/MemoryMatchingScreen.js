73eb59042e43aa3ce1e803e3d76cc711318d858e
Rajesh-Chimple <rajesh@chimple.org>
2018-01-29 16:41:49 +0530

    Stories: 138 and 139 commit
    ->Display the cards as flipped down.
    ->On press, flip card open. Keep it open till another one is flipped open. If both are same, then disappear them. If not, flip them back down

--- app/screens/MemoryMatchingScreen.js
+++ app/screens/MemoryMatchingScreen.js
@@ -4,36 +4,148 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
-  Animated
+  TouchableHighlight,
+  Animated,
+  Alert
 } from 'react-native';
-import ButtonFlip from '../../components/games/ButtonFlip';
+import Board from '../components/games/Board';
+import Board from '../components/games/Card';
 
 export default class MemoryMatchingScreen extends Component {
-  _renderMatrix() {
-    const array = [];
-    for(let i=0; i<4; i++)
-    { let inputRow = [];
-      for(let j=0; j<3; j++){
-        inputRow.push(<View><Animatedbasic/></View>);
-      }
-    array.push(<View style={styles.matrixContainer}>{inputRow}</View>)
-    }
-      return array;
+  constructor(props) {
+    super(props);
+    this.state = {board: new Board(4 , 4), players: 1}
   }
 
-render() {
+  onRestartPress() {
+    alert("RestartGame!!!");
+  }
+
+  makeSinglePlayer() {
+    alert("makeSinglePlayer!!!");
+  }
+
+  getPlayerToggleButtons() {
   return (
-  <View>{this._renderMatrix()}</View>
-);
+      <View style={styles.playerToggleButtons}>
+          <TouchableHighlight onPress={this.makeSinglePlayer} underlayColor="transparent" activeOpacity={0.5}>
+              <Text style={styles.buttonText}>👤 Single Player</Text>
+          </TouchableHighlight>
+      </View>
+  );
 }
+
+handleCardPress(url: string, row: number, col: number) {
+  var board = this.state.board;
+  var previous = board.selected;
+  var selected = this.refs['card' + row + col];
+  var current = {
+      url: url,
+      node: selected
+  }
+
+  if (!previous) {
+      // first card
+      board.selected = current;
+  } else if (previous.url === url) {
+      // successful hit
+      previous.node.setPaired();
+      selected.setPaired();
+
+      this.setState({board: board.pair()});
+  } else {
+      // missed hit
+      board.miss(true);
+
+      setTimeout(
+          () => {
+              selected.hide();
+              previous.node.hide();
+              this.setState({board: board});
+          },
+        100
+      );
+  }
+}
+
+onCardHide() {
+    this.state.board.unlock();
+  }
+
+  canShow() {
+    return !this.state.board.isLocked;
+  }
+
+  render() {
+
+    var board = this.state.board;
+    console.log(this.state.board);
+    var rows = board.grid.map((cards, row) =>
+    <View key={'row' + row} ref={'row' + row} style={styles.row}>
+        {cards.map((cardCfg, col) =>
+          <Card
+            key={'col' + col}
+            ref={'card' + row + col}
+            img={cardCfg.url}
+            onPress={this.handleCardPress.bind(this, cardCfg.url, row, col)}
+            onHide={this.onCardHide}
+            canShow={this.canShow}
+            cardCfg={cardCfg}
+          />
+        )}
+      </View>
+  );
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Memory Game</Text>
+
+        {this.getPlayerToggleButtons()}
+
+        <View style={styles.board}>
+          {rows}
+        </View>
+
+        <TouchableHighlight
+            onPress={this.onRestartPress}
+            underlayColor="transparent"
+            activeOpacity={0.5}>
+            <Text style={styles.buttonText}>🔄 Restart</Text>
+          </TouchableHighlight>
+    </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  matrixContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      paddingTop: 10,
-    },
-
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F4F9CB',
+    width: '100%',
+    height: '100%'
+  },
+  title: {
+    fontFamily: 'ChalkboardSE-Bold',
+    fontSize: 39,
+    marginBottom: 0,
+    color: '#535659',
+  },
+  buttonText: {
+    fontFamily: 'ChalkboardSE-Bold',
+    fontSize: 16,
+    marginTop: 25,
+    color: '#535659',
+  },
+  playerToggleButtons: {
+    flexDirection: 'row',
+  },
+  board: {
+    padding: 5,
+    backgroundColor: 'transparent',
+    borderRadius: 10,
+  },
+  row: {
+    flexDirection: 'row',
+  }
 });
