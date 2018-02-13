bf9edc229b769e69bbcccc00230a1157be8c8299
Anuj <anuj@sutara.org>
2018-02-13 16:59:05 +0530

    Data is coming and also score screen is coming

--- app/components/games/TapHome.js
+++ app/components/games/TapHome.js
@@ -6,20 +6,24 @@ import Confirm from './Confirm';
 import { isPortrait, isLandscape, isTablet } from './Platform';
 import ScoreScreen from '../../screens/ScoreScreen';
 import Tile from './Tile';
+import { isAbsolute } from 'path';
 
 let timerId; 
-let width;
 let iterate = 0;
-let len = 0;
-let j = 0, i = 0;
+let j = 0;
 
 export default class TapHome extends Component {
 
   constructor(props) {
-
     super(props);
     this.state = this._initBoard(props)
 
+    Dimensions.addEventListener('change', () => {
+      this.setState({
+        width : this.fontSizer(this.props.style.height * 0.225) + 20
+      })
+     
+    });
   }
 
   _initBoard = (props) => {
@@ -32,13 +36,23 @@ export default class TapHome extends Component {
     let len = options.length;
     let score = 0;
     let count = 0;
+    let answer = props.data.answer;
+    let width = this.fontSizer(this.props.style.height * 0.225) + 20;
+    let statuses = 'inVisible';
    
     return ({
       options,
       count,
       len,
-      score
+      score,
+      answer,
+      width,
+      statuses
     })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.props.runIndex != nextProps.runIndex && this.setState(this._initBoard(nextProps))
   }
 
 
@@ -52,151 +66,102 @@ export default class TapHome extends Component {
   componentDidMount() {
     //this.setState({len: this.props.data.serial.length})
     //This will start timer and will update text value
-    timerId = setInterval(this.timer, 1400)
-    width = this.props.style.height * 0.225
+    timerId = setInterval(this.timer, 1400) 
 
   }
 
   //This will generate random number and will check on tap condition
   GenerateRandomNumber = (id , view) => {
     
-    if (this.props.data.answer == this.state.options[this.state.count]) {
+    if (this.state.answer == this.state.options[this.state.count]) {
       this.props.onScore(2)
-      view.zoomIn(500).then((endState) => console.log(endState.finished ? 'bounce finished' : 'bounce cancelled'));
+      this.props.onEnd();
+      this.setState({ statuses: 'visible'});
+      console.log(this.state.count);
      
-      if(this.state.score == 9 )
-      {
-        this.setState({
-          numberHolder: this.state.numberHolder,
-          showModal: true,
-        })
-      }
-      else{
-        this.setState({
-          count: 0,
-          score: this.state.score + 1,
-        })
-      }
 
-      //condition for increasing speed
-      if (this.state.score > 1 && this.state.score <= 3) {
-        clearInterval(timerId);
-        timerId = setInterval(this.timer, 1400);
-      }
-      else if (this.state.score > 3 && this.state.score <= 6) {
-        clearInterval(timerId);
-        timerId = setInterval(this.timer, 1000);
-      }
-      else if (this.state.score > 6 && this.state.score <= 9) {
-        clearInterval(timerId)
-        timerId = setInterval(this.timer, 800);
-      }
-      else if (this.state.score > 9 && this.state.score <= 13) {
-        clearInterval(timerId);
-        timerId = setInterval(this.timer, 600);
-      }
-      else if (this.state.score > 13) {
-        clearInterval(timerId);
-        timerId = setInterval(this.timer, 500);
-      }
+    //   //condition for increasing speed
+    //   if (this.state.count > 1 && this.state.count <= 3) {
+    //     clearInterval(timerId);
+    //     timerId = setInterval(this.timer, 1200);
+    //   }
+    //   else if (this.state.count > 3 && this.state.count <= 5) {
+    //     clearInterval(timerId);
+    //     timerId = setInterval(this.timer, 1000);
+    //   }
+    //   else if (this.state.count > 5 && this.state.count <= 7) {
+    //     clearInterval(timerId)
+    //     timerId = setInterval(this.timer, 800);
+    //   }
+    //   else if (this.state.count > 7 && this.state.count <= 10) {
+    //     clearInterval(timerId);
+    //     timerId = setInterval(this.timer, 600);
+    //   }
     }
     else {
-      view.shake(500).then((endState) => console.log(endState.finished ? 'bounce finished' : 'bounce cancelled'));
       iterate = iterate + 1;
       this.setState({ count: 0 })
 
     }
   }//end of generateRandomNumber function
 
-  // onAccept() {
-  //   this.setState({
-  //     numberHolder: 4,
-  //     score: 0,
-  //     count: 2,
-  //     showModal: false,
-  //   });
-  //   clearInterval(timerId);
-  //   timerId = setInterval(this.timer, 2000);
-  // }
-
-  // onDecline() {
-  //   this.setState({ showModal: false });
-  // }
+  fontSizer (screenWidth) {
+    if(screenWidth > 100 && screenWidth < 150){
+      return 40;
+    }else if(screenWidth < 100){
+      return 30;
+    }else if(screenWidth < 200 && screenWidth > 150 ){
+      return 60
+    }else if(screenWidth < 300 && screenWidth > 200 ){ 
+      return 80;
+    }
+  }
 
   render() {
-    console.log(this.props.data.serial); 
+    const { container, subText } = styles;
 
-   
- 
-    const { container, circle, text, subText, scoreText } = stylesPotrait;
+    const cellSize = Math.min(
+      Math.floor(this.props.style.width / 2),
+      Math.floor(this.props.style.height / 2)
+    )
+
+
+    const padding = Math.floor(cellSize * .05)
+    const tileSize = cellSize - padding * 2
+
     return (
-
       <View style={container}>
-          <Tile
-            id={1}
-            onPress={this.GenerateRandomNumber}
-            text={this.props.data.answer}
-            edgeColor='white'
-            style={{
-              width: 80,
-              height: 80,
-              position: 'relative',
-            }}
-            onRender={this._renderTile}
-          />
+        <Tile
+          id={1}
+          onPress={this.GenerateRandomNumber}
+          text={this.state.answer}
+          edgeColor='white'
+          statuses={this.state.statuses}
+          onStatusChange={this._onStatusChange}
+          style={{
+            width: tileSize ,
+            height: tileSize ,
+           
+          }}
+          onRender={this._renderTile}
+        />
         <TouchableOpacity onPress={this.GenerateRandomNumber}>
-            <Text style={[ subText, { fontSize: fontSizer(width) + 20}]}>
+          <Text style={[subText, { fontSize: this.state.width }]}>
             {this.state.options[this.state.count]}
-            </Text>
+          </Text>
         </TouchableOpacity>
       </View>
     );
   }
+
+  _onStatusChange(id, view, prevStatus, currentStatus) {
+    console.log('onstatuschange:', prevStatus, currentStatus)
+    currentStatus == 'visible' && view.zoomIn(250)
+  }
+
 }//End of class 
 
-_renderTile = (id, view) => {
-  //this.state.letters[id] && view.zoomIn(250)
-}
-
-_clickTile = (id, view) => {
-  if (this.state.letters[id] == this.props.data.serial[this.state.currentIndex]) {
-    this.props.onScore(2)
-    this.props.setProgress((this.state.currentIndex + 1) / this.props.data.serial.length)
-    view.zoomOut(250).then((endState) => {
-      if (this.state.currentIndex + 1 >= this.props.data.serial.length) {
-        this.props.onEnd()
-      } else {
-        this.setState((prevState, props) => {
-          const newLetters = prevState.letters.map((value, index) => {
-            return index == id ? prevState.shuffledData[prevState.currentIndex + SIZE * SIZE] : value
-          })
-          return {
-            letters: newLetters,
-            shuffledData: prevState.shuffledData,
-            currentIndex: prevState.currentIndex + 1
-          }
-        })
-        this.state.currentIndex + SIZE * SIZE <= this.props.data.serial.length && view.zoomIn(250)
-      }
-    })
-  } else {
-    view.shake(250)
-  }
-}
-
-function fontSizer (screenWidth) {
-  if(screenWidth > 100 && screenWidth < 150){
-    return 40;
-  }else if(screenWidth < 100){
-    return 30;
-  }else if(screenWidth < 200 && screenWidth > 150 ){
-    return 60
-  }else if(screenWidth < 300 && screenWidth > 200 ){ 
-    return 80;
-  }
-}
-
-const stylesPotrait = {
+const styles = {
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -208,6 +173,6 @@ const stylesPotrait = {
     color: 'white',
     justifyContent: 'center',
     alignSelf: 'center',
-    marginTop: '50%'
+    marginTop: 30
   },
 };//End of styles
