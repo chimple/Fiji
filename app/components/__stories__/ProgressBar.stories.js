a993d405f3d5fef85ab880498668b3a06f704a70
Srikanth Talapadi <sri@zoolore.org>
2018-02-16 17:28:29 +0530

    Changed animation of progressBar to use nativeDriver

---
+++ app/components/__stories__/ProgressBar.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import ProgressBar from '../ProgressBar'
+
+storiesOf('ProgressBar', module)
+  .add('25%', () => (
+    <ProgressBar
+      style={{width: 200}}
+      initialProgress={0.25}
+      progress={0.5}
+      duration={1000}
+    />
+  ))
