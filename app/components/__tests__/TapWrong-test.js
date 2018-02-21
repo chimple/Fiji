790b3d7dd7c9413a369f741cf00761173ffd5e1c
Deepak.Nair <35517589+deepaknair-chimple@users.noreply.github.com>
2018-02-20 20:29:13 -0800

    test case

---
+++ app/components/__tests__/TapWrong-test.js
@@ -0,0 +1,30 @@
+import 'react-native';
+import React from 'react';
+import { Dimensions } from 'react-native'
+import TapWrongGridcomponent from '../games/TapWrongGridComponent';
+//import { titles } from '../../../config/jest/mockData'
+import renderer from 'react-test-renderer';
+import { View } from 'react-native-animatable';
+const window = Dimensions.get("window")
+it('renders correctly', () => {
+    const tree = renderer.create(
+      <TapWrongGridcomponent 
+      data={{
+          word:['D','O','G'],
+          others:['X']
+      }}
+      style = {{
+        width: window.width,
+        height: window.height
+      }}
+     
+      
+      >
+       
+      </TapWrongGridcomponent>
+    ).getInstance()
+   
+    tree.onButtonPress(1)
+    expect(tree).toMatchSnapshot()
+  })
+  
