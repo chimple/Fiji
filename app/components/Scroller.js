ad1671486050e031b1576cbd312428f76ac15492
Srikanth Talapadi <sri@zoolore.org>
2018-02-22 10:14:08 +0530

    One kind of lnf

--- app/components/Scroller.js
+++ app/components/Scroller.js
@@ -1,94 +1,254 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, FlatList, Dimensions, Image, PanResponder } from 'react-native'
-import ScrollableTabView from 'react-native-scrollable-tab-view'
-import FacebookTabBar from './FacebookTabBar'
-import { titles } from '../../config/jest/mockData'
+import { View, Text, SectionList, Dimensions, Animated, TouchableOpacity, Modal } from 'react-native'
+import { Buffer } from 'buffer'
+import SvgUri from 'react-native-svg-uri'
+import LottieView from 'lottie-react-native';
 
-const window = Dimensions.get('window')
-const imageDimensions = {
-  height: window.height,
-  width: window.width
+class AnimationView extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('AnimationView()')
+    this.state = {
+      progress: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    console.log('AnimationView.componentDidMount')
+    Animated.timing(this.state.progress, {
+      toValue: 1,
+      duration: 2000,
+    }).start();
+  }
+  render() {
+    return (
+      <LottieView style={this.props.style}
+        source={this.props.animationCharacter}
+        progress={this.state.progress} />
+    )
+  }
 }
 
+class Dialog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      imageMode: true
+    }
+  }
+  _renderCharacter = (character, animation) => {
+    return (
+      <View style={{ height: 100, width: 100 }}>
+        <TouchableOpacity onPress={() => this._onPressCharacter()}>
+          {this.state.imageMode
+            ?
+            <SvgUri
+              width={32}
+              height={32}
+              svgXmlData={character}
+            />
+            :
+            <AnimationView
+              style={{ height: 80, width: 80, margin: 10 }}
+              animationCharacter={animation}
+            />
+          }
+        </TouchableOpacity>
+      </View>
+    )
+  }
 
-class MyListItem extends React.PureComponent {
-  _onPress = () => {
-    this.props.onPressItem(this.props.id);
-  };
+  _renderText = (text) => (
+    <Text
+      style={{
+        paddingHorizontal: 10,
+        paddingVertical: 20,
+        borderRadius: 8,
+        backgroundColor: 'white'
+      }}
+    >
+      {text}
+    </Text>
+  )
+
+  _onPressCharacter = () => {
+    this.setState({ imageMode: !this.state.imageMode })
+  }
 
   render() {
-    const textColor = this.props.selected ? "red" : "black";
     return (
-      <TouchableOpacity onPress={this._onPress}>
-        <View style={{ height: 100 }}>
-          <Text style={{ color: textColor }}>
-            {this.props.title}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    );
+      <View style={[styles.itemContainer, { backgroundColor: this.props.dialog.color }]}>
+        {this.props.dialog.speaker && (this.props.character.left ?
+          <View style={styles.leftItem}>
+            {this._renderCharacter(this.props.character.character, this.props.animation)}
+            {this._renderText(this.props.dialog.text)}
+          </View>
+          :
+          <View style={styles.rightItem}>
+            {this._renderText(this.props.dialog.text)}
+            {this._renderCharacter(this.props.character.character, this.props.animation)}
+          </View>
+        )}
+      </View>
+    )
+
   }
 }
 
 export default class Scroller extends React.PureComponent {
   constructor(props) {
     super(props)
+    const dialogs = props.data.pages.map(
+      (page, pageIndex) => ({
+        title: Buffer.from(page.bg, 'base64').toString('utf8'),
+        data: page.dialog.map((val, index) => (
+          {
+            ...val,
+            color: page.color,
+            key: String(pageIndex) + ':' + String(index)
+          }
+        ))
+      })
+    )
     this.state = {
-      selected: (new Map(): Map<string, boolean>),
-      num_items: 1,
       refreshing: false,
-      data: props.data[0]
-    };
-  
+      currentIndex: 0,
+      pageIndex: 0,
+      dialogs,
+      visibleDialogs: [{ data: [dialogs[0].data[0]], title: dialogs[0].title }]
+    }
+    this.characters = {}
+    let left = false
+    for (const key in props.data.characters) {
+      left = !left
+      this.characters[key] = {
+        character: Buffer.from(props.data.characters[key], 'base64').toString('utf8'),
+        left
+      }
+    }
+    this.animations = {}
+    for (const key in props.data.animations) {
+      this.animations[key] =
+        JSON.parse(Buffer.from(props.data.animations[key], 'base64').toString('utf8'))
+    }
   }
 
-  _keyExtractor = (item, index) => item.id;
-
-  _onPressItem = (id: string) => {
-    // updater functions are preferred for transactional updates
-    this.setState((state) => {
-      // copy the map rather than modifying state.
-      const selected = new Map(state.selected);
-      selected.set(id, !selected.get(id)); // toggle
-      return { selected, num_items: state.num_items };
-    });
-  };
+  componentDidUpdate() {
+    this.refs.sectionList.scrollToLocation({
+      sectionIndex: this.state.pageIndex,
+      itemIndex: this.state.currentIndex,
+      viewPosition: 1
+    })
+  }
 
   _renderItem = ({ item }) => (
-    <MyListItem
-      id={item.id}
-      key={item.id}
-      onPressItem={this._onPressItem}
-      selected={!!this.state.selected.get(item.id)}
-      title={item.title}
+    <Dialog
+      dialog={item}
+      character={this.characters[item.speaker]}
+      animation={this.animations[item.speaker]}
     />
-  );
+  )
+
+  _renderSectionHeader = ({ section }) => {
+    const w = Dimensions.get("window").width
+    console.log(Dimensions.get("window"))
+    return (
+      <SvgUri
+        style={{ height: 41 }}
+        width={w}
+        height={w}
+        svgXmlData={section.title}
+      />
+    )
+  }
 
   _onRefresh = () => {
     this.setState({ ...this.state, refreshing: true })
     setTimeout(() => {
-      this.setState({
-        ...this.state,
-        data: {...this.state.data, data: [{id: 1, title: '1'},...this.state.data.data]},
+      this.setState((state) => ({
+        ...state,
+        currentIndex: state.currentIndex + 1,
+        visibleDialogs: [{
+          ...state.dialogs[state.currentIndex + 1],
+          id: String(state.currentIndex + 1)
+        }].concat(state.visibleDialogs),
         refreshing: false
-      })
-    }, 5000)
+      }))
+    }, 500)
+  }
+
+  _onPress = () => {
+    if (this.state.currentIndex + 1 >= this.state.dialogs[this.state.pageIndex].data.length &&
+      this.state.pageIndex + 1 >= this.state.dialogs.length) {
+      return
+    }
+    this.setState((state) => {
+      const nextCurrentIndex = state.currentIndex + 1 < state.dialogs[state.pageIndex].data.length
+        ?
+        state.currentIndex + 1
+        :
+        0
+      const nextPageIndex = nextCurrentIndex == 0
+        ?
+        state.pageIndex + 1
+        :
+        state.pageIndex
+      const newState = {
+        ...state,
+        currentIndex: nextCurrentIndex,
+        pageIndex: nextPageIndex,
+        visibleDialogs: state.dialogs.slice(0,nextPageIndex+1).map((page, pageIndex) => (
+          {
+            title: page.title,
+            data: page.data.filter((val, index) => pageIndex < nextPageIndex || index <= nextCurrentIndex)
+          }
+        ))
+      }
+      console.log(newState)
+      return newState
+    })
   }
 
   render() {
     return (
-      <FlatList
-        id={this.state.data.id}
-        key={this.state.data.id}
-        ref="scroller"
-        tabLabel={this.state.data.tabLabel}
-        data={this.state.data.data}
-        extraData={this.state}
-        renderItem={this._renderItem}
-        inverted={true}
-        onRefresh={this._onRefresh}
-        refreshing={this.state.refreshing}
-      />
+      <View style={{
+        flex: 1
+      }}>
+        <SectionList
+          ref="sectionList"
+          sections={this.state.visibleDialogs}
+          renderItem={this._renderItem}
+          renderSectionHeader={this._renderSectionHeader}
+          renderSectionFooter={this._renderSectionHeader}
+          stickySectionHeadersEnabled={true}
+          extraData={this.state.currentIndex}
+          // inverted={true}
+        />
+        <TouchableOpacity onPress={this._onPress}>
+          <Text>Next</Text>
+        </TouchableOpacity>
+      </View>
     )
+  }
+}
+
+const styles = {
+  itemContainer: {
+    padding: 20,
+    flex: 1,
+    justifyContent: 'center',
+    minHeight: 80
+  },
+  leftItem: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
+  },
+  rightItem: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end'
   }
 }
