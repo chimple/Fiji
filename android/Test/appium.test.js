084c0cf3d5b6ec1f3cf7fb61bf22c524387a965c
Prakhar Kumar <prakhar@zoolore.org>
2018-02-27 11:54:10 +0530

    appium

--- android/Test/appium.test.js
+++ android/Test/appium.test.js
@@ -1,7 +1,6 @@
 import wd from 'wd';
-
-
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+// import Dimensions from 'Dimensions';
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 const PORT = 4723;
 const config = {
   platformName: 'Android',
@@ -18,20 +17,33 @@ beforeAll(async () => {
 })
 
 
-// test('page scroll', async () => {
+// test('Get height & width',async()=>{
+//   await driver.sleep(25000)
+//   let {width, height} = Dimensions.get('screen')
+//   console.log(width,height)
+//   });
+      
+//       test('swipe in chat',async()=>{
+//         await driver.sleep(13000)
+//         let {width, height} = Dimensions.get('screen')
+//    const action = new wd.TouchAction(driver);
+//         action.press({ x: width, y: height }).wait(1000).moveTo({ x: width, y: height/2 }).release();
+//         driver.performTouchAction(action); 
+  
+//   });
+  
 
-//   driver.execute('mobile: scroll', {direction: 'down', element: element.value.'ADD-USER'});
-//   driver.performTouchAction(action);
 
-// });
+
 
 test('touchAction', async () => {
-    await driver.sleep(15000);
-    action.press({ x: 375 , y: 1765 }).wait(500).moveTo({ x: 345 , y: 100 }).release();
-    driver.performTouchAction(action);
+  await driver.sleep(15000);
+  action.press({ x: 375 , y: 1765 }).wait(500).moveTo({ x: 345 , y: 100 }).release();
+  driver.performTouchAction(action);
 
 });
-  
+
+
 test('add user', async () => {
   await driver.sleep(10000);
   expect(await driver.hasElementByAccessibilityId('ADD-USER')).toBe(true);
